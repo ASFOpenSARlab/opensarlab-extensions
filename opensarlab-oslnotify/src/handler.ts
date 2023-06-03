b90863aa4c1db20032713416bb6d0e876781f469
Eric Lundell <emlundell@alaska.edu>
2023-06-02 16:04:55 -0800

    Fix endpoint paths

--- opensarlab-oslnotify/src/handler.ts
+++ opensarlab-oslnotify/src/handler.ts
@@ -17,7 +17,7 @@ export async function requestAPI<T>(
   const settings = ServerConnection.makeSettings();
   const requestUrl = URLExt.join(
     settings.baseUrl,
-    'oslnotify', // API Namespace
+    'opensarlab-oslnotify', // API Namespace
     '?type=' + endPoint
   );
 
