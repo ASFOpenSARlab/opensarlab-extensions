61e0bed5d81ea115ede30a5a498f49c42862587a
Eric Lundell <emlundell@alaska.edu>
2023-07-13 15:23:46 -0800

    Doc link

--- v4/opensarlab_doc_link/src/index.ts
+++ v4/opensarlab_doc_link/src/index.ts
@@ -1,18 +1,42 @@
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin
+  JupyterFrontEndPlugin,
 } from '@jupyterlab/application';
 
-/**
- * Initialization data for the opensarlab_doc_link extension.
- */
-const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'opensarlab_doc_link:plugin',
-  description: 'A JupyterLab extension.',
+import { 
+  DOMUtils 
+} from '@jupyterlab/apputils';
+
+import { Widget } from '@lumino/widgets';
+
+class DocsAnchorWidget extends Widget {
+  constructor() {
+    super();
+
+    this.hyperlink = document.createElement('a');
+    this.hyperlink.text = 'OpenSARlab Docs';
+    this.hyperlink.href = 'https://opensarlab-docs.asf.alaska.edu/user-guides/how_to_run_a_notebook/';
+    this.hyperlink.target = 'blank';
+    this.addClass('docs-anchor-widget');
+
+    this.node.appendChild(this.hyperlink);
+  }
+
+  readonly hyperlink: HTMLAnchorElement;
+}
+
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-topbar-doclink',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension opensarlab_doc_link is activated!');
+    
+    let rank = 1100;
+
+    const docLinkWidget = new DocsAnchorWidget();
+    docLinkWidget.id = DOMUtils.createDomID();
+    app.shell.add(docLinkWidget, 'top', {rank:rank});
+
   }
 };
 
-export default plugin;
+export default extension;
