e3b9c4b8e038ad82baba5ca212436cecc9db07de
Eric Lundell <emlundell@alaska.edu>
2023-07-31 18:40:02 -0800

    Add link to gifcap.dev

--- v4/opensarlab_frontend/src/index.ts
+++ v4/opensarlab_frontend/src/index.ts
@@ -7,6 +7,7 @@ import { main as controlbtn } from './components/controlbtn';
 import { main as doc_link } from './components/doc_link';
 import { main as profile_label } from './components/profile_label'
 import { main as oslnotify } from './components/oslnotify';
+import { main as gifcap_btn } from './components/gifcap_btn';
 
 /**
  * Initialization data for the opensarlab_frontend extension.
@@ -19,6 +20,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
     console.log('JupyterLab extension opensarlab_frontend is activated!');
 
     // second arg is rank on topbar
+    gifcap_btn(app, 1025)
     profile_label(app, 1050);
     doc_link(app, 1100);
     controlbtn(app, 1110);
