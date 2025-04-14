2aeda4269795409f825ae5a15a26dcb2f68d750a
segallagher <segallagher@alaska.edu>
2025-04-14 10:14:17 -0800

    refactor: apply osl linting

--- dev-build.sh
+++ dev-build.sh
@@ -2,48 +2,46 @@
 
 set -ex
 
-## 1. 
+## 1.
 ## THIS var needs to be updated to the extension name
 EXTENSION_NAME="opensarlab_frontend"
 
 ## 2.
 JLPM_DEPENDS=(
-    ## THIS array holds extra jlpm node packages to be install
-    @jupyterlab/apputils
-    @jupyterlab/application-extension
-    toastr
-    @lumino/widgets
+	## THIS array holds extra jlpm node packages to be install
+	@jupyterlab/apputils
+	@jupyterlab/application-extension
+	toastr
+	@lumino/widgets
 )
 
 ## 3.
 JLPM_DEV_DEPENDS=(
-    ## THIS array holds extra dev jlpm node packages to be install
-    @types/toastr
+	## THIS array holds extra dev jlpm node packages to be install
+	@types/toastr
 )
 
 ## 4.
 ENV_VARS=(
-    ## THIS array holds environment vars injected into JupyterLab at startup
-    # Escape single quotes ( ' => \' )
-    OPENSARLAB_PROFILE_NAME=\'SAR 1\'
-    OPENSCIENCELAB_LAB_SHORT_NAME=\'opensarlab-test\'
-    OPENSCIENCELAB_PORTAL_DOMAIN=\'https://opensciencelab-test.asf.alaska.edu\'
+	## THIS array holds environment vars injected into JupyterLab at startup
+	# Escape single quotes ( ' => \' )
+	OPENSARLAB_PROFILE_NAME=\'SAR 1\'
+	OPENSCIENCELAB_LAB_SHORT_NAME=\'opensarlab-test\'
+	OPENSCIENCELAB_PORTAL_DOMAIN=\'https://opensciencelab-test.asf.alaska.edu\'
 )
 
 ## 5.
 ## If there is a server "backend" to the extension, say "true"
 USE_SERVER_EXTENSION=true
 
-
 ## 6.
+# shellcheck disable=SC2034
 MAMBA_DEPENDS=(
-    ## THIS array holds extra mamba packages to be installed in the environment. This should be used sparingly
+	## THIS array holds extra mamba packages to be installed in the environment. This should be used sparingly
 )
 
 ## 7.
 ## ANY possible additional python packages will need to be added to the "project.dependencies" section of pyproject.toml
-
-
 
 ##############################################################################
 ##############################################################################
@@ -51,23 +49,23 @@ MAMBA_DEPENDS=(
 # DON'T UPDATE ANY OF THE FOLLOWING
 #
 ##############################################################################
-############################################################################## 
+##############################################################################
 
 MAMBA_ENV_NAME="opensarlab-extension-$EXTENSION_NAME"
-printf "Extension name: $EXTENSION_NAME"
+printf "Extension name: %s" "$EXTENSION_NAME"
 
 # Create environment
-if ! { mamba env list | grep -w "$MAMBA_ENV_NAME";} 
-then
-    mamba create -n $MAMBA_ENV_NAME -y --override-channels --strict-channel-priority -c conda-forge "jupyterlab>=4" "nodejs>=18"
-else 
-    echo "******** Environment $MAMBA_ENV_NAME already exists....updating"
-    mamba update -n $MAMBA_ENV_NAME -y --override-channels --strict-channel-priority -c conda-forge "jupyterlab>=4" "nodejs>=18"
+if ! { mamba env list | grep -w "$MAMBA_ENV_NAME"; }; then
+	mamba create -n $MAMBA_ENV_NAME -y --override-channels --strict-channel-priority -c conda-forge "jupyterlab>=4" "nodejs>=18"
+else
+	echo "******** Environment $MAMBA_ENV_NAME already exists....updating"
+	mamba update -n $MAMBA_ENV_NAME -y --override-channels --strict-channel-priority -c conda-forge "jupyterlab>=4" "nodejs>=18"
 fi
 
 mamba env list
 
-cat > steps.sh <<EOF
+# shellcheck disable=SC2116
+cat >steps.sh <<EOF
 
     set -ex
 
@@ -136,7 +134,7 @@ cat > steps.sh <<EOF
     mamba env list
 
     printf "\n\n"
-    ( $(echo ${ENV_VARS[@]}) jupyter lab -y )
+    ( $(echo "${ENV_VARS[@]}") jupyter lab -y )
 
 EOF
 
--- opensarlab_frontend/handles/profile_label.py
+++ opensarlab_frontend/handles/profile_label.py
@@ -3,11 +3,12 @@ import os
 
 import logging
 
-logging.basicConfig(format='%(filename)s %(asctime)s %(message)s')
-
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
+
+logging.basicConfig(format="%(filename)s %(asctime)s %(message)s")
+
 
 class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
@@ -18,12 +19,13 @@ class RouteHandler(APIHandler):
         """
         /opensarlab-frontend/opensarlab-profile-label
         """
-        profile_name = os.environ.get('OPENSARLAB_PROFILE_NAME', '')
+        profile_name = os.environ.get("OPENSARLAB_PROFILE_NAME", "")
 
         if not profile_name:
-            logging.warning("Environ variable 'OPENSARLAB_PROFILE_NAME' not found.") 
+            logging.warning("Environ variable 'OPENSARLAB_PROFILE_NAME' not found.")
 
         self.finish(json.dumps({"data": profile_name}))
+
 
 def setup_handlers(base_url, url_path=None):
     route_pattern = url_path_join(base_url, "opensarlab-profile-label")
