65fa90116f1a5b1adf7db95c21873c68054e5dfb
segallagher <segallagher@alaska.edu>
2025-04-14 09:12:19 -0800

    refactor: run jlpm linting

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
 import { main as controlbtn } from './components/controlbtn';
 import { main as doc_link } from './components/doc_link';
-import { main as profile_label } from './components/profile_label'
+import { main as profile_label } from './components/profile_label';
 import { main as oslnotify } from './components/oslnotify';
 import { main as gifcap_btn } from './components/gifcap_btn';
 import { main as diskspace } from './components/diskspace';
@@ -20,23 +20,24 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'A JupyterLab extension.',
   autoStart: true,
   optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
-  
+  activate: (
+    app: JupyterFrontEnd,
+    settingRegistry: ISettingRegistry | null
+  ) => {
     // Wait for the application to be restored and
     // for the settings for this plugin to be loaded
-    if(!settingRegistry) {
-      console.log("Settings not found. opensarlab_frontend cannot be established.")
-      return; 
+    if (!settingRegistry) {
+      console.log(
+        'Settings not found. opensarlab_frontend cannot be established.'
+      );
+      return;
     }
 
-    Promise
-      .all([
-          app.restored, 
-          settingRegistry.load(plugin.id)
-      ])
+    Promise.all([app.restored, settingRegistry.load(plugin.id)])
       .then(([, settings]) => {
-
-        async function loadSettings(allSettings: ISettingRegistry.ISettings): Promise<void> {
+        async function loadSettings(
+          allSettings: ISettingRegistry.ISettings
+        ): Promise<void> {
           await gifcap_btn(app, allSettings);
           await profile_label(app, allSettings);
           await doc_link(app, allSettings);
@@ -51,10 +52,12 @@ const plugin: JupyterFrontEndPlugin<void> = {
         // Listen for your plugin setting changes using Signal
         settings.changed.connect(loadSettings);
 
-        console.log("JupyterLab extension opensarlab_frontend is fully operational!");
+        console.log(
+          'JupyterLab extension opensarlab_frontend is fully operational!'
+        );
       })
-      .catch((reason) => {
-        console.error(`Something went wrong...${reason}`)
+      .catch(reason => {
+        console.error(`Something went wrong...${reason}`);
       });
   }
 };
